7aa0189861dcdad45f8242fde99a116832553ef3
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-03-31 01:26:27 -0500

    Complete JS exercise 05, 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  if (
+    end < 0 ||
+    start < 0 ||
+    !Number.isInteger(end) ||
+    !Number.isInteger(start)
+  ) {
+    return "ERROR";
+  }
+  if (end < start) {
+    [start, end] = [end, start];
+  }
+  let sum = start;
+  for (let i = start + 1; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
