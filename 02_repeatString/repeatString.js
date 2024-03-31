60a64440b712c57b646d60935c8921473544e579
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-03-31 00:20:07 -0500

    Complete js exercise 01, 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, amount) {
+  if (amount < 0) {
+    return "ERROR";
+  }
+  let final = "";
+  for (let i = 0; i < amount; i++) {
+    final += string;
+  }
+  return final;
 };
 
 // Do not edit below this line
