fb206ce8f72f50282b88795c4ed7b95e72542e1e
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-03-31 00:45:40 -0500

    Complete JS exercise 003, 004

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...targets) {
+  let i = arr.length;
+  while (i--) {
+    if (targets.includes(arr[i])) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
