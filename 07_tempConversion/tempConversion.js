2a24a119a949c094536d00bee4f7f5de2052199f
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-03-31 01:33:32 -0500

    Complete JS exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (deg) {
+  let result = ((deg - 32) * 5) / 9;
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (deg) {
+  let result = (deg * 9) / 5 + 32;
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
