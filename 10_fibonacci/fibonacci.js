dc3f56846dd07cf043dc81f6698a4e6966f80c7e
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-02 19:13:42 -0500

    Complete JS exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  n = parseInt(n);
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  let previous = 1;
+  let current = 1;
+  let next = 0;
+  for (let i = 3; i <= n; i++) {
+    next = previous + current;
+    previous = current;
+    current = next;
+  }
+  return current;
 };
 
 // Do not edit below this line
