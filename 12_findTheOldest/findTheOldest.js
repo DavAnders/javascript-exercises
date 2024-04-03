9b47b43462c265091ebb01840ffa9c423aa06cf1
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-02 20:02:52 -0500

    Complete JS exercises 11, 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    const currentYear = new Date().getFullYear();
+    const oldestAge =
+      ("yearOfDeath" in oldest ? oldest.yearOfDeath : currentYear) -
+      oldest.yearOfBirth;
+    const currentAge =
+      ("yearOfDeath" in currentPerson
+        ? currentPerson.yearOfDeath
+        : currentYear) - currentPerson.yearOfBirth;
+    return oldestAge > currentAge ? oldest : currentPerson;
+  });
 };
 
 // Do not edit below this line
